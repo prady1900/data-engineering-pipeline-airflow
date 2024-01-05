2c0ca6d6e07d6afbe6b44242aea32d332494eabb
unknown <pradnyeshmali19@gmail.com>
2024-01-06 00:54:46 +0530

    Added ml dag

---
+++ dags/ml_model.py
@@ -0,0 +1,66 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
+import joblib
+import os
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVR
+import pandas as pd
+
+
+def load_data():
+    pg_hook = PostgresHook.get_hook('postgres_connect')
+    df = pg_hook.get_pandas_df(sql='select impact,severity,priority,category,resolve_time from incident_new')
+    df = pd.DataFrame(df.to_json(index=False))
+    my_feat = df[['impact','severity','priority','category','resolve_time']]
+    df_new = pd.get_dummies(my_feat,columns=['category'])
+    tg_value = df['resolve_time']
+
+    x_train, x_test, y_train, y_test = train_test_split(
+        df_new, tg_value, random_state=106, test_size=0.2, shuffle= True
+    )
+    
+    model_svr = SVR(kernel='linear')
+    model_svr.fit(x_train,y_train)
+    if(os.path.exists('./ml_model_file/res_time_predictor.pkl')):
+        os.remove('./ml_model_file/res_time_predictor.pkl')
+        joblib.dump(model_svr, './ml_model_file/res_time_predictor.pkl')
+    else:
+        joblib.dump(model_svr, './ml_model_file/res_time_predictor.pkl')
+    
+
+    
+
+
+
+default_args = {
+    'owner':'prady1900',
+    'depends_on_past':False,
+    'start_date': datetime(2023,1,1),
+    'retries':'2',
+    'retry_delay': timedelta(minutes=2),
+    'email_on_retry': False,
+    'email_on_failure': False
+
+}
+with DAG (
+    'ml_dag',
+    default_args=default_args,
+    schedule_interval='@daily',
+    catchup=False
+)as ml_dag:
+    start_task = EmptyOperator(
+        task_id='start_task',
+
+    )
+    load_data_from_idea = PythonOperator(
+        task_id = 'load_data_from_idea',
+        python_callable=load_data
+    )
+
+    start_task>>load_data_from_idea
+
+
